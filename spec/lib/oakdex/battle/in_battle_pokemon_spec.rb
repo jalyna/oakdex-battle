e0026ad214a95ac73861e0784a6306a4176db396
Jalyna <jalyna.schroeder@gmail.com>
2019-01-21 11:05:44 +0100

    fix in battle pokemon spec

--- spec/lib/oakdex/battle/in_battle_pokemon_spec.rb
+++ spec/lib/oakdex/battle/in_battle_pokemon_spec.rb
@@ -1,87 +1,58 @@
 require 'spec_helper'
 
 describe Oakdex::Battle::InBattlePokemon do
-  let(:species) { Oakdex::Pokedex::Pokemon.find('Pikachu') }
-  let(:iv) do
-    {
-      hp: 10,
-      atk: 10,
-      def: 10,
-      sp_atk: 10,
-      sp_def: 10,
-      speed: 10
-    }
+  let(:pokemon) do
+    double(:pokemon, {
+      hp: 17,
+      atk: 9,
+      def: 7,
+      sp_atk: 9,
+      sp_def: 8,
+      speed: 12
+    })
   end
-  let(:ev) do
-    {
-      hp: 10,
-      atk: 10,
-      def: 10,
-      sp_atk: 10,
-      sp_def: 10,
-      speed: 10
-    }
-  end
-  let(:move) do
-    Oakdex::Battle::Move.new(
-      Oakdex::Pokedex::Move.find('Thunder Shock'), 30, 40
-    )
-  end
-  let(:additional_attributes) { {} }
-  let(:attributes) do
-    {
-      exp: 100,
-      gender: 'female',
-      ability: Oakdex::Pokedex::Ability.find('Static'),
-      nature: Oakdex::Pokedex::Nature.find('Bashful'),
-      hp: 12,
-      iv: iv,
-      ev: ev,
-      moves: [move]
-    }.merge(additional_attributes)
-  end
-  subject { described_class.new(species, attributes) }
 
-  describe '.create' do
-    let(:species_name) { 'Bulbasaur' }
-    let(:options) { { level: 10 } }
-    let(:pokemon) { double(:pokemon) }
-    let(:species) { double(:species) }
+  subject { described_class.new(pokemon) }
 
-    before do
-      allow(Oakdex::Pokedex::Pokemon).to receive(:find!)
-        .with(species_name).and_return(species)
-      allow(Oakdex::Battle::PokemonFactory).to receive(:create)
-        .with(species, options).and_return(pokemon)
-    end
-
-    it 'creates pokemon with auto-generated attributes' do
-      expect(described_class.create(species_name, options)).to eq(pokemon)
+  %w[
+    species
+    types
+    trainer
+    trainer=
+    name
+    gender
+    moves
+    current_hp
+    wild?
+    item_id
+    amie
+    amie_level
+    traded?
+    moves_with_pp
+    change_hp_by
+    change_pp_by
+    level
+    fainted?
+  ].each do |method|
+    describe "##{method}" do
+      it 'is forwarded from pokemon' do
+        value = double(:value)
+        expect(pokemon).to receive(:"#{method}").and_return(value)
+        expect(subject.public_send(method)).to eq(value)
+      end
     end
   end
 
-  describe '#name' do
-    it { expect(subject.name).to eq('Pikachu') }
+  describe '#accuracy' do
+    it { expect(subject.accuracy).to eq(1) }
   end
 
-  describe '#species' do
-    it { expect(subject.species).to eq(species) }
+  describe '#evasion' do
+    it { expect(subject.evasion).to eq(1) }
   end
 
-  describe '#moves' do
-    it { expect(subject.moves).to eq([move]) }
-  end
-
-  describe '#gender' do
-    it { expect(subject.gender).to eq('female') }
-  end
-
-  describe '#current_hp' do
-    it { expect(subject.current_hp).to eq(12) }
-  end
-
-  describe '#level' do
-    it { expect(subject.level).to eq(4) }
+  describe '#critical_hit_prob' do
+    it { expect(subject.critical_hit_prob).to eq(Rational(1, 24)) }
   end
 
   describe '#hp' do
@@ -120,53 +91,7 @@ describe Oakdex::Battle::InBattlePokemon do
     end
   end
 
-  describe '#accuracy' do
-    it { expect(subject.accuracy).to eq(1) }
-  end
 
-  describe '#evasion' do
-    it { expect(subject.evasion).to eq(1) }
-  end
-
-  describe '#critical_hit_prob' do
-    it { expect(subject.critical_hit_prob).to eq(Rational(1, 24)) }
-  end
-
-  describe '#moves_with_pp' do
-    it { expect(subject.moves_with_pp).to eq([move]) }
-
-    context 'no pp' do
-      let(:move) do
-        Oakdex::Battle::Move.new(
-          Oakdex::Pokedex::Move.find('Thunder Shock'), 0, 40
-        )
-      end
-
-      it { expect(subject.moves_with_pp).to eq([]) }
-    end
-  end
-
-  describe '#change_hp_by' do
-    let(:change_by) { -2 }
-    before { subject.change_hp_by(change_by) }
-
-    it { expect(subject.current_hp).to eq(10) }
-
-    context 'hp under 0' do
-      let(:change_by) { -30 }
-      it { expect(subject.current_hp).to eq(0) }
-    end
-
-    context 'positive' do
-      let(:change_by) { 2 }
-      it { expect(subject.current_hp).to eq(14) }
-    end
-
-    context 'more than max' do
-      let(:change_by) { 200 }
-      it { expect(subject.current_hp).to eq(17) }
-    end
-  end
 
   describe '#reset_stats' do
     let(:initial_stat) { 100 }
@@ -174,10 +99,8 @@ describe Oakdex::Battle::InBattlePokemon do
       subject.change_stat_by(:atk, 2)
       subject.change_stat_by(:def, -3)
       subject.change_stat_by(:evasion, 3)
-      allow(Oakdex::Battle::PokemonStat).to receive(:initial_stat)
-        .with(:atk, anything).and_return(initial_stat)
-      allow(Oakdex::Battle::PokemonStat).to receive(:initial_stat)
-        .with(:def, anything).and_return(initial_stat)
+      allow(pokemon).to receive(:atk).and_return(initial_stat)
+      allow(pokemon).to receive(:def).and_return(initial_stat)
     end
 
     it 'resets stats' do
@@ -193,8 +116,7 @@ describe Oakdex::Battle::InBattlePokemon do
     let(:stat) { :atk }
     let(:initial_stat) { 100 }
     before do
-      allow(Oakdex::Battle::PokemonStat).to receive(:initial_stat)
-        .with(stat, anything).and_return(initial_stat)
+      allow(pokemon).to receive(stat).and_return(initial_stat)
     end
 
     it 'returns true when value was changed' do
@@ -213,7 +135,7 @@ describe Oakdex::Battle::InBattlePokemon do
       before { subject.change_stat_by(stat, change_by) }
       it {
         expect(subject.atk).to eq((initial_stat *
-        Oakdex::Battle::PokemonStat::STAGE_MULTIPLIERS[-2]).to_i)
+        Oakdex::Battle::InBattlePokemon::STAGE_MULTIPLIERS[-2]).to_i)
       }
 
       context 'accuracy' do
@@ -221,7 +143,7 @@ describe Oakdex::Battle::InBattlePokemon do
 
         it {
           expect(subject.accuracy)
-            .to eq(Oakdex::Battle::PokemonStat::STAGE_MULTIPLIERS_ACC_EVA[-2])
+            .to eq(Oakdex::Battle::InBattlePokemon::STAGE_MULTIPLIERS_ACC_EVA[-2])
         }
       end
 
@@ -230,7 +152,7 @@ describe Oakdex::Battle::InBattlePokemon do
 
         it {
           expect(subject.evasion)
-            .to eq(Oakdex::Battle::PokemonStat::STAGE_MULTIPLIERS_ACC_EVA[-2])
+            .to eq(Oakdex::Battle::InBattlePokemon::STAGE_MULTIPLIERS_ACC_EVA[-2])
         }
       end
 
@@ -240,7 +162,7 @@ describe Oakdex::Battle::InBattlePokemon do
 
         it {
           expect(subject.critical_hit_prob)
-            .to eq(Oakdex::Battle::PokemonStat::
+            .to eq(Oakdex::Battle::InBattlePokemon::
               STAGE_MULTIPLIERS_CRITICAL_HIT[1])
         }
       end
@@ -308,121 +230,6 @@ describe Oakdex::Battle::InBattlePokemon do
       expect(subject.status_conditions).to eq([status_condition])
       subject.remove_status_condition(status_condition)
       expect(subject.status_conditions).to be_empty
-    end
-  end
-
-  describe '#change_pp_by' do
-    let(:change_by) { -1 }
-    let(:move_name) { 'Thunder Shock' }
-    before { subject.change_pp_by(move_name, change_by) }
-
-    it { expect(move.pp).to eq(29) }
-
-    context 'pp under 0' do
-      let(:change_by) { -40 }
-      it { expect(move.pp).to eq(0) }
-    end
-
-    context 'positive' do
-      let(:change_by) { 1 }
-      it { expect(move.pp).to eq(31) }
-    end
-
-    context 'more than max' do
-      let(:change_by) { 200 }
-      it { expect(move.pp).to eq(40) }
-    end
-
-    context 'unknown move' do
-      let(:move_name) { 'Struggle' }
-      it { expect(move.pp).to eq(30) }
-    end
-  end
-
-  %i[types].each do |field|
-    describe "##{field}" do
-      it {
-        expect(subject.public_send(field))
-        .to eq(species.public_send(field))
-      }
-    end
-  end
-
-  describe '#wild?' do
-    it { expect(subject).not_to be_wild }
-
-    context 'wild' do
-      let(:additional_attributes) { { wild: true } }
-      it { expect(subject).to be_wild }
-    end
-  end
-
-  describe '#original_trainer' do
-    it { expect(subject.original_trainer).to be_nil }
-
-    context 'original trainer given' do
-      let(:additional_attributes) { { original_trainer: 'Name of Trainer' } }
-      it { expect(subject.original_trainer).to eq('Name of Trainer') }
-    end
-  end
-
-  describe '#traded?' do
-    it { expect(subject).not_to be_traded }
-
-    context 'trainer given' do
-      let!(:trainer) { Oakdex::Battle::Trainer.new('Awesome Trainer', [subject]) }
-      it { expect(subject).not_to be_traded }
-
-      context 'original trainer given' do
-        let(:additional_attributes) { { original_trainer: 'Name of Trainer' } }
-        it { expect(subject).to be_traded }
-
-        context 'ot is same as trainer' do
-          let(:additional_attributes) { { original_trainer: 'Awesome Trainer' } }
-          it { expect(subject).not_to be_traded }
-        end
-      end
-    end
-  end
-
-  describe '#item_id' do
-    it { expect(subject.item_id).to be_nil }
-
-    context 'item given' do
-      let(:additional_attributes) { { item_id: 'Name of Item' } }
-      it { expect(subject.item_id).to eq('Name of Item') }
-    end
-  end
-
-  describe '#amie' do
-    it { expect(subject.amie).to eq({
-      affection: 0,
-      fullness: 0,
-      enjoyment: 0
-    }) }
-
-    context 'amie given' do
-      let(:additional_attributes) { { amie: {
-        affection: 1,
-        fullness: 2,
-        enjoyment: 3
-      } } }
-      it { expect(subject.amie).to eq({
-        affection: 1,
-        fullness: 2,
-        enjoyment: 3
-      }) }
-    end
-  end
-
-  describe '#amie_level' do
-    it { expect(subject.amie_level(:affection)).to eq(0) }
-
-    context 'amie given' do
-      let(:additional_attributes) { { amie: {
-        affection: 201
-      } } }
-      it { expect(subject.amie_level(:affection)).to eq(4) }
     end
   end
 end
